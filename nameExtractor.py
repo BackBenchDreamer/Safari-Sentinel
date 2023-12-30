769e7236d9cf737bbdf567ff0aeb3dceb041cfa6
Utpal Kumar <73326144+UtpalKuma-r@users.noreply.github.com>
2023-12-30 12:01:29 +0530

    done

--- nameExtractor.py
+++ nameExtractor.py
@@ -1,6 +1,7 @@
 def extractName(list):
     nlist = [x.split(' ')[-1] for x in list if len(x.split(' ')[-1])>0]
-    return nlist
+    rlist = [x+'s' if x[-1] != 's' else x for x in nlist]
+    return rlist
 
 if __name__ == '__main__':
     print('Code to be imported in different file.')
