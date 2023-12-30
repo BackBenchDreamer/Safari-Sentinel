ffbfb36b75fd7212153ced9b7c56fb4ade6430d7
Utpal Kumar <73326144+UtpalKuma-r@users.noreply.github.com>
2023-12-30 15:50:31 +0530

    Update for 2 word animal name

--- nameExtractor.py
+++ nameExtractor.py
@@ -1,5 +1,5 @@
 def extractName(list):
-    nlist = [x.split(' ')[-1] for x in list if len(x.split(' ')[-1])>0]
+    nlist = [' '.join([y for y in x.split(' ') if not y.isdigit()]) for x in list if len(x.split(' ')[-1])>0]
     rlist = [x+'s' if x[-1] != 's' else x for x in nlist]
     return rlist
 
